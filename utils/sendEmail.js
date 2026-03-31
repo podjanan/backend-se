ff186ac98a598f1cfe8d282ab50eff6ed7531af3
podjanan.o <podjanan12300@gmail.com>
2026-03-31 21:48:27 +0700

    connect supabase

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,11 +1,21 @@
 const nodemailer = require('nodemailer')
 
-module.exports = async(email,otp)=>{
-    const transporter = nodemailer.createTransport({service:"gmail",auth:{user:process.env.EMAIL_USER,pass:process.env.EMAIL_PASS}})
+module.exports = async (email, otp) => {
+  const transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
+    family: 4, // 🔥 แก้ ENETUNREACH ตรงนี้
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS,
+    },
+  })
 
-    await transporter.sendMail({
-        to:email,
-        subject:"YourCamp : OTP Verification",
-        text:`Your OTP is ${otp}`
-})
+  await transporter.sendMail({
+    from: process.env.EMAIL_USER,
+    to: email,
+    subject: "YourCamp : OTP Verification",
+    text: `Your OTP is ${otp}`,
+  })
 }
